d54b0b993dd4e463637907de16bd868df9725464
Juan Ignacio Troisi <juanignacio.troisi@muttdata.ai>
2024-08-12 09:47:52 -0300

    Comment run_vars

--- single_run/run_vars.py
+++ single_run/run_vars.py
@@ -12,19 +12,16 @@ CV_N_SPLITS = 5 # Amount of cross validation splits to prevent overfitting
 GRID_SEARCH_ITERS = 30 # Amount of RandomGridSearch iterations
 NUM_AUGMENTATIONS = 20 # # If using data augmentation in the corrector
 
+# Default splits of the Informer
 SPLIT_TRAIN_PROPORTION = 0.67
 SPLIT_VAL_PROPORTION = 0.13
 SPLIT_RANDOM_STATE = 42
 
-# Raw data identifier
-DATA_DATE = "20240801" # Next date to the last date of the dataset
+DATA_DATE = "20240801" # Raw data identifier. Next date to the last date of the dataset
 
-# Directory where raw data is stored
-SIGNALS_DIR = "data/logtel"
-# Raw data filename
-SIGNAL_FILENAME = f"cantidad_entregas_total_{DATA_DATE}.csv"
+SIGNALS_DIR = "data/logtel" # Directory where raw data is stored
+SIGNAL_FILENAME = f"cantidad_entregas_total_{DATA_DATE}.csv" # Raw data filename
 
-# Model run identified
-DATA_PARAM = f"single_run_lgbm_total_{DATA_DATE}"
+DATA_PARAM = f"single_run_lgbm_total_{DATA_DATE}" # Model run identified
 # Directory where to store results
 RESULTS_PATH = f"results/informer_{DATA_PARAM}_ftS_sl{FEATURES_SEQ_LEN}_ll7_pl{PRED_SEQ_LEN}_dm512_nh8_el2_dl1_df2048_atprob_fc5_ebtimeF_dtTrue_mxTrue_test_0"
